390134e3a3c85495f67a58c4d5a29c8f620e7541
Sandhyae436 <80383303+Sandhyae436@users.noreply.github.com>
2021-07-08 16:33:02 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -73,8 +73,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("", callback_data = "start"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("Channel", url='https://t.me/joinchat/tRrBCnvMsTwxMWQ9'),
+                    InlineKeyboardButton("Group", url='https://t.me/Troll_Cinemas_Group'),
                 ]
             ]
         )
